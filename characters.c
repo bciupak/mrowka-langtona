f848a07a3be0815824815dc5238da8715cef4663
bciupak <153275462+bciupak@users.noreply.github.com>
2023-12-28 23:13:41 +0100

    Dodanie znaków potrzebnych do rysowania planszy

---
+++ characters.c
@@ -0,0 +1,18 @@
+#include "characters.h"
+
+wchar_t* LINE_VERTICAL = L"│";
+wchar_t* LINE_HORIZONTAL = L"─";
+wchar_t* LINE_DOWN_RIGHT = L"┌";
+wchar_t* LINE_DOWN_LEFT = L"┐";
+wchar_t* LINE_UP_RIGHT = L"└";
+wchar_t* LINE_UP_LEFT = L"┘";
+wchar_t* SQUARE_WHITE = L" ";
+wchar_t* SQUARE_BLACK = L"█";
+wchar_t* ARROW_NORTH_WHITE = L"△";
+wchar_t* ARROW_NORTH_BLACK = L"▲";
+wchar_t* ARROW_EAST_WHITE = L"▷";
+wchar_t* ARROW_EAST_BLACK = L"▶";
+wchar_t* ARROW_SOUTH_WHITE = L"▽";
+wchar_t* ARROW_SOUTH_BLACK = L"▼";
+wchar_t* ARROW_WEST_WHITE = L"◁";
+wchar_t* ARROW_WEST_BLACK = L"◀";
---
+++ characters.h
@@ -0,0 +1,27 @@
+#ifndef CHARACTERS
+#define CHARACTERS
+
+#include <wchar.h>
+#include <stdio.h>
+
+// border and "Ant" characters
+extern wchar_t* LINE_VERTICAL;
+extern wchar_t* LINE_HORIZONTAL;
+extern wchar_t* LINE_DOWN_RIGHT;
+extern wchar_t* LINE_DOWN_LEFT;
+extern wchar_t* LINE_UP_RIGHT;
+extern wchar_t* LINE_UP_LEFT;
+extern wchar_t* SQUARE_WHITE;
+extern wchar_t* SQUARE_BLACK;
+extern wchar_t* ARROW_NORTH_WHITE;
+extern wchar_t* ARROW_NORTH_BLACK;
+extern wchar_t* ARROW_EAST_WHITE;
+extern wchar_t* ARROW_EAST_BLACK;
+extern wchar_t* ARROW_SOUTH_WHITE;
+extern wchar_t* ARROW_SOUTH_BLACK;
+extern wchar_t* ARROW_WEST_WHITE;
+extern wchar_t* ARROW_WEST_BLACK;
+
+
+#endif // CHARACTERS
+// use %ls to print wchar_t*
